f30c7cb491fc6b2015ceccb9a9924bc4b69938b7
Daniel Rojo Pérez <danielrojo@gmail.com>
2024-07-03 18:08:16 +0200

    counter with xpath

---
+++ cypress/e2e/counter.cy.ts
@@ -0,0 +1,15 @@
+require('@cypress/xpath');
+
+describe('counter test', () => {
+    it('passes', () => {
+        cy.visit('localhost:3000/counter')
+        //check if the counter is 0
+        cy.get('h2').should('have.text', '0')
+        // get element with xpath: /html/body/div/button
+        cy.xpath('/html/body/div/button').click()
+        // wait for the button to be clicked
+        // cy.wait(500)
+        // check if the counter is 1
+        cy.get('h2').should('have.text', '1')
+    })
+})
